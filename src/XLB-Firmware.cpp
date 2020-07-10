442b07d6a2fc58f43dea0ff6cb0d78cd6c8fb17a
aut0mat3d <aut0mat3d@users.noreply.github.com>
2020-07-10 08:45:36 +0200

    ...

--- src/XLB-Firmware.cpp
+++ src/XLB-Firmware.cpp
@@ -16,13 +16,14 @@ MCP_CAN CAN(MCP_CS_SPI);
 
 //const char XLBPreamble[] = "CM";
 
-/*DIESES STRUCT SOLL IN DIE GLOBALVARIABLES
+/*DIESES STRUCT SOLL IN DIE GLOBALVARIABLES*/
 struct XLBCANMsg
 {
   INT32U Id;
   INT8U Len;
   INT8U Data[8];
 };
+
 
 //bool errorprinted = false;
 
